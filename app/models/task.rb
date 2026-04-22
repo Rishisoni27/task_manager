09cf07a82ac669fad56902e6874fa5df4bbea115
Rishi Soni <rishisoni1116@gmail.com>
2026-04-22 12:05:14 +0530

    added calbacks and validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,8 +1,33 @@
 class Task < ApplicationRecord
+  # callbacks 
+  before_validation :set_default_status
+  before_save :formate_title
+  after_create :log_creation
+  after_destroy :log_delete
+
+  # Validations 
   validates :title, presence: true ,length: {minimum:3},uniqueness: true
   # validates :title, length: {minimum:3}
   validates :description, presence: true
   validates :status, presence: true
   # ,inclusion: {in: %w[pending , compleated]}
-  # ya to status pending ho ga ya Compleated 
+  # ya to status pending ho ga ya Compleated
+  
+  private 
+   def set_default_status
+    self.status ||= "pending"
+   end
+
+   def format_title
+    self.title = title.strip.capitalize
+   end 
+
+   def log_creation
+    puts "Task '#{title}' create successfully"
+   end 
+   
+   def log_delete
+    puts "task '#{title}' deleted   "
+   end 
+
 end
